d7c75d3ff30a0886bcb959d478eb4361f9b744a7
DIR\jakub.romel <jakub.romel@accenture.com>
2019-08-30 16:52:48 +0200

    add hp loss animation

--- scenes/ui/hp_bar/hp_bar.gd
+++ scenes/ui/hp_bar/hp_bar.gd
@@ -7,9 +7,11 @@ export(bool) var flip = false
 
 var progress_bar : TextureProgress
 var hp_label : Label
+var tween_animation : Tween
 
 
 func _ready() -> void:
+    tween_animation = $Tween
     progress_bar = $TextureProgress
     if flip:
         progress_bar.fill_mode = TextureProgress.FILL_RIGHT_TO_LEFT
@@ -24,5 +26,8 @@ func assign_initial_values() -> void:
     
 
 func on_hp_loss(damage_take_args : DamageTakenArgs) -> void:
+    $AnimationPlayer.play("hp_loss")
+    tween_animation.interpolate_property(progress_bar, "value", damage_take_args.current_hp, damage_take_args.target_hp, 0.6, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+    tween_animation.start()
     progress_bar.value = damage_take_args.target_hp
     hp_label.text = str(damage_take_args.target_hp)
